ee4b217de1871c88239adc5f8c3bb9ec0f97facb
sumibhatta <sumibhatta37@gmail.com>
2020-12-08 09:57:24 +0545

    Requirements- Open a window

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <GL/glut.h>
-#include <iostream>
-using namespace std;
 
+//Initalization
 void init(){
-    glClearColor(0.769,0.656,0.5,1.0);
+    glClearColor(0.769,0.656,0.5,1.0);  //Set Background Color other than default
 }
+
+//Function which basically displays what we want to in window
 void displayThis(){
-glClear(GL_COLOR_BUFFER_BIT);
-glFlush();
+glClear(GL_COLOR_BUFFER_BIT); // Clear the screen (Works like a duster)
+    
+    /*My Art will be here ;p */
+    
+glFlush(); //Render all the output to the screen
 }
 
+//Main function
 int main(int argc, char**argv){
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB);
+    
+    glutInit(&argc, argv);  //initalize the toolkit
+    glutInitDisplayMode(GLUT_RGB); //Initalize the display mode to RGBA model
 
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(500,500);
+    glutInitWindowSize(500, 500);  //Initalize window size
+    glutInitWindowPosition(500,500); //Initalize window position
 
-    glutCreateWindow("Graphics");
-    glutDisplayFunc(displayThis);
-    init();
-    glutMainLoop();
+    glutCreateWindow("Graphics"); //Create Window from the above properties and give it a name
+    init(); //Call initalization function
+    glutDisplayFunc(displayThis); //call displayThis() function to render sth. on screen   
+    glutMainLoop(); //Run the perpetual loop so that window won't disappear in an instant
     }
