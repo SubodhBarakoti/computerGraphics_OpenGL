482cf6cc5a28b8c91521898d8c0ed2edf7edb50a
sumibhatta <sumibhatta37@gmail.com>
2021-03-04 20:50:29 +0545

    ◣

---
+++ triangle.cpp
@@ -0,0 +1,24 @@
+#include <GL/glut.h>
+
+//Function which basically displays what we want to in window
+void displayThis(){
+
+glClear(GL_COLOR_BUFFER_BIT);      // Clear the screen (Works like a duster)
+    glBegin(GL_TRIANGLES);        // Create a triangle
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex2f(-0.5, 0.5);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glEnd();
+glFlush();                  //Render all the output to the screen
+
+}
+
+//Main function
+int main(int argc, char**argv){
+
+    glutInit(&argc, argv);          //initalize the toolkit
+    glutCreateWindow("Graphics");  //Create Window from the above properties and give it a name
+    glutDisplayFunc(displayThis); //call displayThis() function to render sth. on screen
+    glutMainLoop();              //Run the perpetual loop so that window won't disappear in an instant
+    }
